de6dbc6e7668ec5dc9e644cc74eb9f89311a9787
5t3ph <thinkdobecreate@gmail.com>
2020-07-08 22:41:18 -0500

    New submission function

--- api/submission-created.js
+++ api/submission-created.js
@@ -1,44 +1,20 @@
-const axios = require("axios");
-
-exports.handler = async (event, context, callback) => {
+require("dotenv").config();
+const fetch = require("node-fetch");
+const { BUTTONDOWN_API_KEY } = process.env;
+exports.handler = async (event) => {
   const { email } = JSON.parse(event.body).payload;
-
-  if (!email) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        error: "Email is required",
-      }),
-    };
-  }
-
-  try {
-    const API_KEY = process.env.BUTTONDOWN_API_KEY;
-    const response = await axios.post(`https://api.buttondown.email/v1/subscribers`, {
-      body: JSON.stringify({ email }),
-      headers: {
-        Authorization: `Token ${API_KEY}`,
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.status >= 400) {
-      callback(null, {
-        statusCode: 400,
-        body: JSON.stringify({
-          error: `There was an error subscribing to the newsletter.`,
-        }),
-      });
-    }
-
-    callback(null, {
-      statusCode: 201,
-      body: JSON.stringify({ error: "" }),
-    });
-  } catch (error) {
-    callback(null, {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message || error.toString() }),
-    });
-  }
+  console.log(`Recieved a submission: ${email}`);
+  return fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`Submitted to Buttondown:\n ${data}`);
+    })
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
