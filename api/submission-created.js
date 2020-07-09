9276813d947f4f188de104785f59cf369f8a5c39
5t3ph <thinkdobecreate@gmail.com>
2020-07-08 22:19:55 -0500

    use axios for function

--- api/submission-created.js
+++ api/submission-created.js
@@ -1,4 +1,4 @@
-import fetch from "isomorphic-unfetch";
+const axios = require("axios");
 
 exports.handler = async (event, context, callback) => {
   const { email } = JSON.parse(event.body);
@@ -14,32 +14,31 @@ exports.handler = async (event, context, callback) => {
 
   try {
     const API_KEY = process.env.BUTTONDOWN_API_KEY;
-    const response = await fetch(`https://api.buttondown.email/v1/subscribers`, {
+    const response = await axios.post(`https://api.buttondown.email/v1/subscribers`, {
       body: JSON.stringify({ email }),
       headers: {
         Authorization: `Token ${API_KEY}`,
         "Content-Type": "application/json",
       },
-      method: "POST",
     });
 
     if (response.status >= 400) {
-      return {
+      callback(null, {
         statusCode: 400,
         body: JSON.stringify({
           error: `There was an error subscribing to the newsletter.`,
         }),
-      };
+      });
     }
 
-    return {
+    callback(null, {
       statusCode: 201,
       body: JSON.stringify({ error: "" }),
-    };
+    });
   } catch (error) {
-    return {
+    callback(null, {
       statusCode: 500,
       body: JSON.stringify({ error: error.message || error.toString() }),
-    };
+    });
   }
 };
