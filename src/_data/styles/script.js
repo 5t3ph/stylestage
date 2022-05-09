71b8ef4461dd2695e960016c1a86019379482ba8
jessica-nagar <77754279+jessica-nagar@users.noreply.github.com>
2022-05-09 19:34:44 -0400

    Delete script.js

--- src/_data/styles/script.js
+++
@@ -1,8 +0,0 @@
-/*
-Style Stage is a modern CSS showcase styled by community contributions.
-Learn more at https://stylestage.dev
-
-This pen is loaded with the source html and styles to get you started creating your own stylesheet to contribute.
-
-Updated: 7/9/2020
-*/
